59731199523a64bd22125502ab4b810eafb85fd1
dilip <mailtome190@gmail.com>
2017-01-24 23:50:16 +0530

    commiting code for Travel Service API

---
+++ src/main/java/com/travel/service/TravelService.java
@@ -0,0 +1,6 @@
+package com.travel.service;
+
+public interface TravelService {
+
+	public void getCustomer();
+}
---
+++ src/main/java/com/travel/service/impl/TravelServiceImpl.java
@@ -0,0 +1,11 @@
+package com.travel.service.impl;
+
+import com.travel.service.TravelService;
+
+public class TravelServiceImpl implements TravelService {
+
+	public void getCustomer() {
+		System.out.println("Hello Travel customer!!!");
+	}
+
+}
